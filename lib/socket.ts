9cb5e462fbade99957a7548dc5c959219b9f62ed
nmChaau <chaunmse180582@fpt.edu.vn>
2026-06-23 23:54:10 +0700

    chore: add socket_url env

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from 'socket.io-client';
 
 // Điểm kết nối socket thực tế lấy theo host backend ở next.config.ts
-const SOCKET_URL = 'https://wash-auto.vercel.app';
+const SOCKET_URL = process.env.SOCKET_URL;
 
 export const socket: Socket = io(SOCKET_URL, {
   autoConnect: false,
